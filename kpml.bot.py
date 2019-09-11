1186c456c46ea4dce96ce4a0243d590a2d8546ce
gera <55020604+sudo-gera@users.noreply.github.com>
2019-09-11 20:31:21 +0300

    Add files via upload
    code

---
+++ kpml.bot.py
@@ -0,0 +1,26 @@
+from urllib.request import urlopen
+from json import loads
+from json import dumps
+from urllib.parse import quote
+from time import sleep
+from time import time
+def api(path,data):
+ sleep(1/3)
+ data=data.encode()
+ return loads(urlopen('https://api.vk.com/method/'+path+'v=5.101&access_token=4a2843c76eb198b26ada0a77f1ffe56319f51a7e4b936226cabdb4060efaabc76177977ce585e91e30a5f',data=data).read().decode())
+
+def get(a=0):
+ q=api('messages.getConversations?count=200&filter=unread&','')
+ q=q['response']['items']
+ q=[[w['conversation']['peer']['id'],w['last_message']['text'],w] for w in q if w['conversation']['can_write']['allowed']]
+ if a==0:
+  q=[w[:2] for w in q]
+ return q
+
+def send(id,text):
+  q=api('messages.send?random_id='+str(int(time()*2**28))+'&user_id='+str(id)+'&','message='+text)
+  print(q)
+
+while 1:
+ for q in get():
+  send(q[0],q[1])

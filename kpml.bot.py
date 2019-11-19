273fe1fb5b808c674ce1853a8db881887541a257
sudo-gera <gera.tatarinov.999@gmail.com>
2019-11-19 09:29:21 +0300

    lookall

--- kpml.bot.py
+++ kpml.bot.py
@@ -49,11 +49,11 @@ def send(text,id=None,key=''):
    id=q[0]
   global d
   global defkey
-  if key!='':
-   key=key[0]
-   key='{"buttons":['+','.join(['['+','.join(['{"color":"'+d[e.split('×')[0]]+'","action":{"type":"text","label":"'+e.split('×')[1]+'"}}' for e in w.split('|')]) +']' for w in key.split('\n') if w])+']}'
-  else:
+  if key=='':
    key=defkey[:]
+  else:
+   key=key[0]
+  key='{"buttons":['+','.join(['['+','.join(['{"color":"'+d[e.split('×')[0]]+'","action":{"type":"text","label":"'+e.split('×')[1]+'"}}' for e in w.split('|')]) +']' for w in key.split('\n') if w])+']}'
   key='&keyboard='+key
   text=str(text)
   sleep(5)

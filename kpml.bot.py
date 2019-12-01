a703fdd769fc7874598e791b8e76b25eedfb2fe6
sudo-gera <gera.tatarinov.999@gmail.com>
2019-12-01 21:04:38 +0300

    send

--- kpml.bot.py
+++ kpml.bot.py
@@ -238,7 +238,7 @@ try:
   for w in db.keys():
    if w.isdigit():
     for e in db[w]['time']:
-     db['time'][w]=w
+     db['time'][e]=w
      if 0 < tn % (24*3600) - int(e) < 300 and tn - db[w]['ls'] >= 300:
       worked=work(db[w]['empty'])
       if worked:

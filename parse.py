54609a95ed718d29afa09e6bc446dd6b02230f7f
sudo-gera <gera.tatarinov.999@gmail.com>
2020-01-26 16:45:42 +0300

    send

--- parse.py
+++ parse.py
@@ -8,16 +8,43 @@ from time import asctime
 
 rmo='января февраля марта апреля мая июня июля августа сентября октября ноября декабря'.split()
 emo='jan feb mar apr may jun jul aug sep oct nov dec'.split()
+beg='Изменения в расписании на '
 
 
 def parse():
- q=urlopen('http://kpml.ru/pages/raspisanie/izmeneniya-v-raspisanii').read().decode()
+ try:
+  q=open('../kpml.bot.html').read()
+ except:
+  q=str(time()-400)+'\x01'
+ bt=q.split('\x01')[0]
+ if time()-float(bt)>300:
+  q=urlopen('http://kpml.ru/pages/raspisanie/izmeneniya-v-raspisanii').read().decode()
+  open('../kpml.bot.html','w').write(str(time())+'\x01'+q)
+ else:
+  q=q.split('\x01')[1]
  q=q.replace('<','\x01\x02').replace('>','\x01').replace('&nbsp;',' ').replace('&lt;','<').replace('&gt;','>').replace('&amp;','&').replace('&quot;','"').replace('&apos;',"'")
- q=q.split('''«Кировский''')[0]
- t=beg
- q=q.split(t)[1:]
+ q=q[:q.index('«Кировский')]
+ q=q[q.index('\x01\x02body'):]
+ q=q.replace('\x01\x02br ','\n\x01\x02br ')
+ q=q.replace('\x01\x02br/\x01','\n')
+ q=q.replace('\x01\x02br ','\n\x01\x02br ')
+ q=q.replace('\x01\x02/p\x01','\x01\x02/p\x01\n')
+ q=q.split('\x01')
+# q=[w if len(w) < 2 or w[0] != '\x02' else ('\x03'+w[2:] if w[:2] in ['\x02/','\x02!'] else ('\x04'+w[1:-1]+'\x04' if w[-1] == '/' else w))  for w in q]
+ q=[w for w in q if w and( w[0] != '\x02' or w[0] == '\x02' and w[-1] == '/')]
+ q=['\x01'+w+'\x01' if w[0] == '\x02' else w for w in q]
+ q=[w for w in q if w]
+ q=''.join(q)
+ q=q.strip()
+ q=q.split(beg)[1:]
  return q
 
+
+def out():
+ q=parse()
+ t='<===========>\n'+beg
+ q=t+t.join(q)
+ return q
 
 def repa(day,mon):
  q=parse()
@@ -31,12 +58,15 @@ def repa(day,mon):
   w[0]=[int(e) for e in w[0] if e]
   if day not in w[0]:
    w[1]=''
- q=[w[1] for w in q]
+ q=[w[1] for w in q if w[1]]
  q='\n'.join(q)
  return q
 
 
+
 def get(clas,day,mon):
+ day=int(day)
+ mon=int(mon)
  text=repa(day,mon)
  '''
 
@@ -54,3 +84,4 @@ print(get('9А','20','0'))  #20 января
 print(get('10Б','1','9'))  #1 октября
 print(get('1А','15','10')) #15 ноября
 print(get('4В','4','11'))  #4 декабря
+print(get('4В','27','0'))  #4 декабря

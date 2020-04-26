6094e247652e370b0c5af4a8fca0d920f1fee224
sudo-gera <gera.tatarinov.999@gmail.com>
2020-04-26 22:07:10 +0300

    start

--- start.py
+++ start.py
@@ -34,7 +34,7 @@ except:
   try:
    for w in admin:
     sleep(1/3)
-    urlopen('https://api.vk.com/method/messages.send?random_id='+str(time()).replace('.','')+'&user_id='+str(w)+'&v=5.101&access_token='+token,data=('message='+q).encode()).read()
+    loads(urlopen('https://api.vk.com/method/messages.send?random_id='+str(time()).replace('.','0')+'&user_id='+str(w)+'&v=5.101&access_token='+token,data=('message='+q).encode()).read().decode())['response']
   except:
    print(q,error())
   open('/'.join(abspath(argv[0]).split('/')[:-1])+'/kpml.bot.error','w').write(str(time())+'\x08'+q)

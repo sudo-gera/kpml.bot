23467c7aaa063e386215ddbc5c7ad311e597c9b8
sudo-gera <gera.tatarinov.999@gmail.com>
2020-06-29 18:49:27 +0300

    format

--- kpml.platform.py
+++ kpml.platform.py
@@ -1,80 +1,95 @@
 
 
-#здесь описаны общие функции для работы с платформами
+# здесь описаны общие функции для работы с платформами
 
-#универсальный класс для платформ, тут описаны общие методы
-#описанием платформы является класс, который наследует от этого
+# универсальный класс для платформ, тут описаны общие методы
+# описанием платформы является класс, который наследует от этого
 class stdplatform:
- def __init__(self):
-  self.name=self.__class__.__name__
-  self.token=self.loadtoken()
- def loadtoken(self):
-  try:
-   return open('../kpml.bot.token_'+self.name).read().strip()
-  except:
-   return ''
- def savetoken(self,token):
-  open('../kpml.bot.token_'+self.name,'w').write(str(token).strip())
+    def __init__(self):
+        self.name = self.__class__.__name__
+        self.token = self.loadtoken()
 
-#для создания новой платформы достаточно создать класс, наследующий от stdplatform и имеющий функции
-#sendsometext(self,text,keyboard,id):
+    def loadtoken(self):
+        try:
+            return open('../kpml.bot.token_' + self.name).read().strip()
+        except BaseException:
+            return ''
+
+    def savetoken(self, token):
+        open('../kpml.bot.token_' + self.name, 'w').write(str(token).strip())
+
+# для создания новой платформы достаточно создать класс, наследующий от stdplatform и имеющий функции
+# sendsometext(self,text,keyboard,id):
 # отправка сообщения
 # если платформа не поддерживает клавиатуры, параметр keyboard можно игнорировать
-#lookforunread():
+# lookforunread():
 # получить список непрочитанных сообщений
-#getlink(self,id):
+# getlink(self,id):
 # получение ссылки по id
-#по мимо них могут содержаться любые вспомогательные функции
-#первым аргументом всегда передаётся объект (self), его рекомендуется использовать для хранения токена: self.token
+# по мимо них могут содержаться любые вспомогательные функции
+# первым аргументом всегда передаётся объект (self), его рекомендуется
+# использовать для хранения токена: self.token
 
-#создание словаря dplats, где будут содержаться информация обо всех платформах
-dplats=dict()
-#обращение к платформе должно происходить через функцию plats которая принимает строку с названием класса платформы в качестве аргумента и возвращает необходимый объект, или список названий платформ, если ничего не передать
-#обращение напрямую к платформе рекомендуется делать только из этого файла, а в остальных использовать обие функции, описанные здесь
-#функция plats записывает в словарь dplats экземпляры классов платформ, каждый экземпляр создаётся единожды за время работы программы
+
+# создание словаря dplats, где будут содержаться информация обо всех платформах
+dplats = dict()
+# обращение к платформе должно происходить через функцию plats которая принимает строку с названием класса платформы в качестве аргумента и возвращает необходимый объект, или список названий платформ, если ничего не передать
+# обращение напрямую к платформе рекомендуется делать только из этого файла, а в остальных использовать обие функции, описанные здесь
+# функция plats записывает в словарь dplats экземпляры классов платформ,
+# каждый экземпляр создаётся единожды за время работы программы
+
+
 def plats(q=None):
- for w in stdplatform.__subclasses__():
-  if w not in dplats:
-   dplats[w.__name__]=w()
- if q==None:
-  return list(dplats.keys())
- else:
-  return dplats[q]
+    for w in stdplatform.__subclasses__():
+        if w not in dplats:
+            dplats[w.__name__] = w()
+    if q is None:
+        return list(dplats.keys())
+    else:
+        return dplats[q]
 
 
-#получить последние сообщения в формате [[id,сообщение,профиль],[id,сообщение,профиль],[id,сообщение,профль]]
+# получить последние сообщения в формате
+# [[id,сообщение,профиль],[id,сообщение,профиль],[id,сообщение,профль]]
 def look():
- ext=[]
- for w in plats():
-  ext+=[[e[0],e[1].lower(),w] for e in plats(w).lookforunread()]
- return ext
+    ext = []
+    for w in plats():
+        ext += [[e[0], e[1].lower(), w] for e in plats(w).lookforunread()]
+    return ext
 
-#функция отправки сообщений, аргументы: текст, клавиатура (по умолчанию: defkey), приниматель(если не указан и функция вызвана во время обработки входящих сообщений, получателем будет тот, чьё сообение обрабатывается
-def send(text,key=None,_id=None,_platform=None):
- text=str(text)
- print(text)
- if _platform==None:
-  global platform
- else:
-  platform=_platform
- if _id==None:
-  global id
- else:
-  id=_id
- if key==None:
-  key=defkey
- profile=db[platform][id]
- key=keygen(key,profile)
- plats(platform).sendsometext(text,key,id)
+# функция отправки сообщений, аргументы: текст, клавиатура (по умолчанию:
+# defkey), приниматель(если не указан и функция вызвана во время обработки
+# входящих сообщений, получателем будет тот, чьё сообение обрабатывается
 
-#отправка сообщения администрации (только в вк)
+
+def send(text, key=None, _id=None, _platform=None):
+    text = str(text)
+    print(text)
+    if _platform is None:
+        global platform
+    else:
+        platform = _platform
+    if _id is None:
+        global id
+    else:
+        id = _id
+    if key is None:
+        key = defkey
+    profile = db[platform][id]
+    key = keygen(key, profile)
+    plats(platform).sendsometext(text, key, id)
+
+# отправка сообщения администрации (только в вк)
+
+
 def log(q):
- print(q)
- for w in admin:
-  send(str(q),defkey,w,'vk')
+    print(q)
+    for w in admin:
+        send(str(q), defkey, w, 'vk')
 
-#создаёт ссылку на пользователя. Формат входа: [id,text,platform] где text не используется и может быть чем угодно
-def getlink(id,platform):
- return plats(platform).getlink(id)
+# создаёт ссылку на пользователя. Формат входа: [id,text,platform] где
+# text не используется и может быть чем угодно
 
 
+def getlink(id, platform):
+    return plats(platform).getlink(id)
--- start.py
+++ start.py
@@ -2,62 +2,60 @@
 
 from urllib.request import urlopen
 from json import loads
-from json import dumps
-from urllib.parse import quote
 from time import sleep
 from time import time
-from time import asctime
 from traceback import format_exc as error
-from os import popen
 from os import system
 from os import chdir
-from random import shuffle
 from os.path import abspath
 from sys import argv
 
 
-#сюда нужно вписать id страниц вк тех, кто должен принимать сообщения об ошибках
-admin=['225847803','382227482']
+# сюда нужно вписать id страниц вк тех, кто должен принимать сообщения об
+# ошибках
+admin = ['225847803', '382227482']
 
-#это обработчик ошибок. Сообщения идут в вк админам. Всё что ниже редактировать только в крайнем случае.
-#считается, что этот код рабоатет, если его не трогать. Поэтому если код работает - не трогать.
-#в файле kpml.bot.py содержится основной исполняемый текст. В случае ошибки, программа ниже обработает это и сообщит об ошибке админам.
-#Об ошибке , которая произошла в коде, который ниже, никто никогда не узнает, но бот перестанет работать.
-#Поэтому сохраняйте этот код без изменений. Это гарант того, что в основном файле можно делать любые ошибки и это не смертельно.
+# это обработчик ошибок. Сообщения идут в вк админам. Всё что ниже редактировать только в крайнем случае.
+# считается, что этот код рабоатет, если его не трогать. Поэтому если код работает - не трогать.
+# в файле kpml.bot.py содержится основной исполняемый текст. В случае ошибки, программа ниже обработает это и сообщит об ошибке админам.
+# Об ошибке , которая произошла в коде, который ниже, никто никогда не узнает, но бот перестанет работать.
+# Поэтому сохраняйте этот код без изменений. Это гарант того, что в
+# основном файле можно делать любые ошибки и это не смертельно.
 
 chdir('/'.join(abspath(argv[0]).split('/')[:-1]))
 try:
- token_vk=open('../kpml.bot.token_vk').read()
-except:
- token_vk=''
-while 1:
- system('git pull --no-edit')
- try:
-  exec(open('./kpml.bot.py').read())
-  q=''
- except:
-  q=str(error())
- if q:
-  try:
-   a=open('../kpml.bot.error').read()
-  except:
-   a=str(time()-400)+'\x08'
-  try:
-   bt=float(a.split('\x08')[0])
-  except:
-   bt=time()-400
-  if time()-float(bt)>100 or '\x08'.join(a.split('\x08')[1:]) != q:
-   try:
-    for w in admin:
-     sleep(1/3)
-     url='https://api.vk.com/method/messages.send?random_id='
-     url+=str(time()).replace('.','0')
-     url+='&user_id='+str(w)+'&v=5.101&access_token='+str(token_vk)
-     data=('message='+str(q)).encode()
-     q=loads(urlopen(url,data=data).read().decode())
-     if 'response' not in q:
-      print(q)
-   except:
-    print(q,error())
-   q=str(q)
-   open('../kpml.bot.error','w').write(str(time())+'\x08'+q)
+    token_vk = open('../kpml.bot.token_vk').read()
+except BaseException:
+    token_vk = ''
+while True:
+    system('git pull --no-edit')
+    try:
+        exec(open('./kpml.bot.py').read())
+        q = ''
+    except BaseException:
+        q = str(error())
+    if q:
+        try:
+            a = open('../kpml.bot.error').read()
+        except BaseException:
+            a = str(time() - 400) + '\x08'
+        try:
+            bt = float(a.split('\x08')[0])
+        except BaseException:
+            bt = time() - 400
+        if time() - float(bt) > 100 or '\x08'.join(a.split('\x08')[1:]) != q:
+            try:
+                for w in admin:
+                    sleep(1 / 3)
+                    url = 'https://api.vk.com/method/messages.send?random_id='
+                    url += str(time()).replace('.', '0')
+                    url += '&user_id=' + \
+                        str(w) + '&v=5.101&access_token=' + str(token_vk)
+                    data = ('message=' + str(q)).encode()
+                    q = loads(urlopen(url, data=data).read().decode())
+                    if 'response' not in q:
+                        print(q)
+            except BaseException:
+                print(q, error())
+            q = str(q)
+            open('../kpml.bot.error', 'w').write(str(time()) + '\x08' + q)
